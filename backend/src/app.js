6e627349c550d9166a5b3dbe9782973af0885abb
Sameer <jangidsameer77@gmail.com>
2026-04-02 00:02:16 +0530

    Implement user registration functionality with password hashing

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,17 @@
 import express from "express";
 import cors from "cors";
-
+import userRoutes from "./routes/userRoutes.js";
 const app = express();
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 
+
 // routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use("/api/users", userRoutes);
 export default app;
---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,44 @@
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+const tokenGenerate = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
+const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "Please fill all the fields" });
+    }
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const user = await User.create({
+      username,
+      email,
+      password,
+    });
+    if (user) {
+      res.status(201).json({
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        token: tokenGenerate(user._id),
+        message: "User registered successfully",
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+export { registerUser };
