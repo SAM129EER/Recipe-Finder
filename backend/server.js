e91204b136db21f23c97cea8cc4f63e663d615b7
Sameer <jangidsameer77@gmail.com>
2026-04-01 23:08:29 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import "./src/config/env.js"; // load env first
+import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
+
+const PORT = process.env.PORT || 5000;
+
+// connect DB first
+connectDB();
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
